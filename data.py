d6caaf76d1083bbb57cd86e8028b4cf866c33d13
Tola Shobande <58945003+TolaJS@users.noreply.github.com>
2024-09-06 22:02:37 +0100

    Initial code draft for the `data.py` file.

--- data.py
+++ data.py
@@ -2,12 +2,36 @@
 author:         Tola Shobande
 name:           data.py
 date:           4/09/2024
-description:    Data loader and data augmentation
+description:    Data loader and data augmentation using Dataset module
 """
 
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
+from torchvision import datasets, transforms as T
 
-class ProjectDataset(Dataset):
-    def __init__(self,data, labels):
+class ProjectDataset():
+    def __init__(self,mode):
+        self.mode = mode
 
-    pass
+    def data_transforms(self):
+        if self.mode == 'train':
+            transforms = T.Compose([
+                T.Resize((224, 224)),
+                T.RandomHorizontalFlip(),
+                T.RandomVerticalFlip(),
+                T.RandomRotation(45),
+                T.ToTensor(),
+                T.ColorJitter(),
+                T.RandomErasing(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+            ])
+            pass
+        elif self.mode == 'val':
+            pass
+
+
+
+    def __getitem__(self, item):
+        pass
+
+
