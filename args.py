0422883b8d35307e3d28b37092f6f33a5d8615fe
Tola Shobande <58945003+TolaJS@users.noreply.github.com>
2024-09-23 23:46:00 +0100

    Minor changes

--- args.py
+++ args.py
@@ -5,14 +5,14 @@ date:           4/09/2024
 description:    ...
 """
 
-dropout = 0.2
+dropout = 0.1
 batch_size = 64
 seed = 7
-lr = 0
-gamma = 0
-epochs = 0
-num_classes = 0
-weight_decay = 0
+lr = 0.01
+gamma = 0.95
+epochs = 10
+num_classes = 11
+weight_decay = 0.0005
 eval_mode = True
 save_path = "./checkpoint/"
 model_path = "./checkpoint/"    # Change this to saved model file path
