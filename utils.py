da75e6a8fca0cc507b32121b88e7ba4aa1e46f5c
Tola Shobande <58945003+TolaJS@users.noreply.github.com>
2024-09-23 23:43:51 +0100

    Updates to the MetricTracker to operate on specified device. Added and updated logging functions

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import torch
 import random
 import torchmetrics
+
 import args
 
 
@@ -27,6 +28,12 @@ class MetricTracker:
         self.top5_accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=num_classes, top_k=5)
         self.f1_score = torchmetrics.F1Score(task="multiclass", num_classes=num_classes, average='macro')
 
+    def to(self, device):
+        self.accuracy = self.accuracy.to(device)
+        self.top5_accuracy = self.top5_accuracy.to(device)
+        self.f1_score = self.f1_score.to(device)
+        return self
+
     def update(self, preds, target):
         self.accuracy.update(preds, target)
         self.top5_accuracy.update(preds, target)
@@ -45,5 +52,14 @@ class MetricTracker:
         self.f1_score.reset()
 
 
-def format_log_message(mode, i, epoch, loss, acc):
-    return f'| Mode:{mode:<5} | Iter:{i:5.1f} | Epoch:{epoch:5.1f}/{args.epochs} | Train_Loss:{loss:8.3f} | Val_Acc{acc:7.3f} |'
+def format_log_message(mode, i, epoch, loss, acc, top1, top5, f1):
+    return f'| Mode:{mode:<5} | Iter:{i:5.1f} | Epoch:{epoch}/{args.epochs} | Train_Loss:{loss:8.3f} | Train_Acc:{acc:7.3f} | Top-1:{top1*100:7.3f} | Top-5:{top5*100:7.3f} | F1-Score:{f1:7.3f}'
+
+
+def print_summary(logger, name, train_size, val_size, test_size):
+    logger.info('*'*17 + 'Summary' + '*'*17)
+    logger.info(f'# model: {name}')
+    logger.info(f'# train size: {train_size}')
+    logger.info(f'# validation size: {val_size}')
+    logger.info(f'# test size: {test_size}')
+    logger.info('*'*41+'\n')
